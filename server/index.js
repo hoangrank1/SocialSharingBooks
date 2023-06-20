024958b8abfde7f0a5911d81d41ee8cdee4fd6d6
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-06-20 10:07:31 +0700

    index
    Set up index.js

---
+++ server/index.js
@@ -0,0 +1,63 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+/* configurations */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({
+  policy: "cross-origin"
+}));
+app.use(morgan("common"));
+app.use(bodyParser.json({
+  limit: "30mb",
+  extended: true
+}));
+app.use(bodyParser.urlencoded({
+  limit: "30mb",
+  extended: true
+}));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+/* File storage */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage });
+
+/* Mongoose setup */
+const MONGO_URL='mongodb+srv://pvhoang20:mitsuriDaisuki@cluster0.invorp6.mongodb.net/?retryWrites=true&w=majority';
+const PORT = 3001;
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(MONGO_URL, {   
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD Data one time */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect`)
+  }); 
