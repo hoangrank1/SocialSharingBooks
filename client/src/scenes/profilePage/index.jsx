73863333de08ffbcf8556508e29f2989f841183a
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-06-23 17:56:47 +0700

    Profile Page Modification
    Set up profile page.

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,8 +1,63 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+import ProfileWidget from "scenes/widgets/ProfileWidget"
+import UserWidget from "scenes/widgets/UserWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+
 const ProfilePage = () => {
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!user) return null;
+
   return (
-    <div>
-      profilepage
-    </div>
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="2rem"
+        justifyContent="center"
+      >
+        <Box 
+          flexBasis={isNonMobileScreens ? "26%" : undefined}
+          paddingTop="2rem"
+        >
+          <UserWidget userId={userId} picturePath={user.picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <ProfileWidget userId={userId} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <Box m="2rem 0" />
+            <FriendListWidget userId={userId} openAddRemove={false} />
+          </Box>
+        )}
+      </Box>
+    </Box>
   );
 };
 
