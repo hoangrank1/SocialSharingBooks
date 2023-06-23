df64c868599bfa6784aded5e2dbd6936068cc8cf
hoangrank1 <75740532+hoangrank1@users.noreply.github.com>
2023-06-23 14:52:39 +0700

    Friend List Widget Modification
    Add Friend List Widget to homepage

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,7 @@ import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
 import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -33,6 +34,7 @@ const HomePage = () => {
 
         {isNonMobileScreens && (
           <Box flexBasis="26%">
+            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
